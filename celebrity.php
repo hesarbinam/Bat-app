ea58f1202757d8bd93c827edd3cab9509e947c7e
Mohsen <72359061+hesarbinam@users.noreply.github.com>
2020-10-22 14:46:53 +0330

    Add files via upload

---
+++ celebrity.php
@@ -0,0 +1,52 @@
+{
+  "celebrity": [
+    {
+      "id": "1",
+      "name": "سجاد تیکه به درخت",
+      "email": "hesarbinam@gmail.com",
+      "image": "http://s16.picofile.com/file/8411549392/IMG_20201021_003712.jpg"
+    },
+    {
+      "id": "1",
+      "name": "فاطمه زهرا تکیه به درخت",
+      "email": "in the home",
+      "image": "http://s16.picofile.com/file/8411549292/IMG_20201016_144840.jpg"
+    },
+    {
+      "id": "1",
+      "name": "فاطمه زهرا نشسته روی صندلی خیابان",
+      "email": "hesarbinam@gmail.com",
+      "image": "http://s14.picofile.com/file/8409890434/IMG_20200922_123144_663.jpg"
+    },
+    {
+      "id": "2",
+      "name": "فاطمه زهرا در سبزه زار",
+      "email": "hesarbinam@gmail.com",
+      "image": "http://s14.picofile.com/file/8409890526/IMG_20200922_123136_155.jpg"
+    },
+    {
+      "id": "2",
+      "name": "سجاد و فاطمه در پارک",
+      "email": "hesarbinam@gmail.com",
+      "image": "http://s15.picofile.com/file/8409890568/IMG_20200922_123146_838.jpg"
+    },
+    {
+      "id": "2",
+      "name": "فاطمه زهرا در پارک",
+      "email": "hesarbinam@gmail.com",
+      "image": "http://s15.picofile.com/file/8409890642/IMG_20200922_123122_358.jpg"
+    },
+    {
+      "id": "2",
+      "name": "فاطمه و سجاد در پارک نشسته روی صندلی",
+      "email": "hesarbinam@gmail.com",
+      "image": "http://s15.picofile.com/file/8409890692/IMG_20200922_123110_287.jpg"
+    },
+    {
+      "id": "2",
+      "name": "علی اصغر",
+      "email": "hesarbinam@gmail.com",
+      "image": "http://s15.picofile.com/file/8409890734/IMG_20200922_123833_781.jpg"
+    }
+  ]
+}
